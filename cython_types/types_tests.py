6cd07fea46e6753fbfb16e4af56f81a2d8771372
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2020-11-25 21:32:22 +0100

    add cython return types tests

---
+++ cython_types/types_tests.py
@@ -0,0 +1,33 @@
+def raise_int(a):
+    print(a)
+    raise RuntimeError
+
+
+def raise_double(a):
+    print(a)
+    raise RuntimeError
+
+
+def raise_bint(a):
+    print(a)
+    raise RuntimeError
+
+
+def raise_list(a):
+    print(a)
+    raise RuntimeError
+
+
+def raise_set(a):
+    print(a)
+    raise RuntimeError
+
+
+def raise_dict(a):
+    print(a)
+    raise RuntimeError
+
+
+def raise_object(a):
+    print(a)
+    raise RuntimeError
